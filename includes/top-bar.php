7e04634ae2f5a2a1c4e9abbd99ef6b3d5e5089b3
Amine Nouri <nouri.webdev@gmail.com>
2026-02-16 21:10:32 +0100

    top bouton

--- includes/top-bar.php
+++ includes/top-bar.php
@@ -5,8 +5,8 @@
 				
     					<span class="text-left full-sc d-none d-md-inline">Vente Abonnement IPTV , VOD et Sharing aux meilleurs prix </span>
     					<span class="text-left full-sc d-inline d-md-none">Abonnement IPTV & VOD</span>
-    					<div class="text-right mx-xs-auto mx-sm-auto mr-md-0">
-    					    <div class="fav-icon"><a href="tel:<?php echo $gsm; ?>" class="phone mobile-call-btn"><i class="fa fa-phone-square"></i><span class="d-none d-md-inline"><?php echo $gsm; ?></span></a></div>
+    					<div class="text-right mx-xs-auto mx-sm-auto mr-md-0 phone-icon-container">
+    					    <div class="fav-icon"><a href="tel:<?php echo $gsm; ?>" class="phone mobile-call-btn"><i class="fa fa-phone-square"></i><!-- <span class="d-none d-md-inline"><?php echo $gsm; ?></span> --></a></div>
     					</div>
 				</div>
 		</div>
